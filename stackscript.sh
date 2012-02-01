9469c197d0b9448e012e29f30977e674c2d99b23
Patrick Lucas <me@patricklucas.com>
2012-02-01 16:53:50 -0500

    Use bash instead of sh

--- stackscript.sh
+++ stackscript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 aptitude install -y curl
-curl -s https://raw.github.com/patricklucas/config/master/bootstrap.sh | sh
+curl -s https://raw.github.com/patricklucas/config/master/bootstrap.sh | bash
