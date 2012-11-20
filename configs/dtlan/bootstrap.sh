2eb10897d09c267538a31fe61a120ab427acdf65
Patrick Lucas <me@patricklucas.com>
2012-11-20 14:36:43 -0500

    Prepare dtlan script for merge into master

--- configs/dtlan/bootstrap.sh
+++ configs/dtlan/bootstrap.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-export CONFIG_BRANCH="multi_config_refactor" # XXX
 export CONFIG_NAME="dtlan"
 apt-get install -y curl
-curl -s "https://raw.github.com/patricklucas/config/$CONFIG_BRANCH/bootstrap.sh" | bash
+curl -s "https://raw.github.com/patricklucas/config/master/bootstrap.sh" | bash
