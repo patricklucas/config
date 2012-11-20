90ff053681fad58f7fa7c95683d23b4f093b2976
Patrick Lucas <me@patricklucas.com>
2012-11-20 14:23:33 -0500

    Actually need bash :|

--- configs/dtlan/bootstrap.sh
+++ configs/dtlan/bootstrap.sh
@@ -3,4 +3,4 @@
 export CONFIG_BRANCH="multi_config_refactor" # XXX
 export CONFIG_NAME="dtlan"
 apt-get install -y curl
-curl -s "https://raw.github.com/patricklucas/config/$CONFIG_BRANCH/bootstrap.sh" | sh
+curl -s "https://raw.github.com/patricklucas/config/$CONFIG_BRANCH/bootstrap.sh" | bash
