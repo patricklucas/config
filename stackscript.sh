5ecb6742948999638bb9e391281a48b1268e82d1
Patrick Lucas <me@patricklucas.com>
2012-11-20 14:36:02 -0500

    Remove old stackscript.sh

--- stackscript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# <udf name="hostname" LABEL="Hostname" />
-# <udf name="private_ip" LABEL="Private IP Address" />
-
-export HOSTNAME
-export PRIVATE_IP
-
-curl -s https://raw.github.com/patricklucas/config/master/bootstrap.sh | bash
